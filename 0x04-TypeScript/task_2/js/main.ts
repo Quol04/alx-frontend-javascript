6c2f59124427f29374eb975925e203ac5e52a94c
quol <kuolamol77@gmail.com>
2025-09-12 12:31:55 +0300

    added task 7

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -70,3 +70,24 @@ console.log(createEmployee(200));   // Teacher
 console.log(createEmployee(1000));  // Director
 console.log(createEmployee("$500")); // Director
 
+
+// ---------------------------------------------
+// Task 8: String literal type Subjects
+// ---------------------------------------------
+type Subjects = "Math" | "History";
+
+// Function teachClass
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  }
+  if (todayClass === "History") {
+    return "Teaching History";
+  }
+  return "";
+}
+
+// Example usage
+
+console.log(teachClass("Math"));    // Teaching Math
+console.log(teachClass("History")); // Teaching History
