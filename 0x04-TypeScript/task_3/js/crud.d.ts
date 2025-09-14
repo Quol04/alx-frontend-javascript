472b2d15a5357cba8de0fc68c51165fd0e884e03
quol <kuolamol77@gmail.com>
2025-09-14 12:15:40 +0300

    adjusted the crud.d.ts file

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -5,3 +5,5 @@ import { RowID, RowElement } from './interface';
 declare function insertRow(row: RowElement): RowID;
 declare function deleteRow(rowId: RowID): void;
 declare function updateRow(rowId: RowID, row: RowElement): RowID;
+
+declare function insertRow(row: RowElement): number;
